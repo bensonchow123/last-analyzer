a7b29b93d1c60ec30eb02f18f67d465e9e40bd24
benson <bensonistaken@gmail.com>
2026-02-28 11:57:59 +0000

    chore(scrobble vault): define api structure

--- backend/scrobble_vault/main.py
+++ backend/scrobble_vault/main.py
@@ -12,22 +12,31 @@ logging.basicConfig(
     format="%(asctime)s [%(levelname)s] %(name)s: %(message)s"
 )
 
+async def start_scrobble_vault():
+    # Run sync on startup
+    await sync_scrobble_vault()
+
+    # Register cron job for sync
+    aiocron.crontab(f'*/{env.SYNC_INTERVAL_MINUTES} * * * *', func=sync_scrobble_vault)
+
+    logging.info(f"Scheduled sync every {env.SYNC_INTERVAL_MINUTES} minutes")
+
+    # Keep the event loop running forever
+    await asyncio.Event().wait()
+
+async def start_api():
+    pass
+
 async def main():
     # Initialize database connection pool
     await init_db()
     logging.info("Database connection pool initialized")
     
     try:
-        # Run sync on startup
-        await sync_scrobble_vault()
-        
-        # Register conjob for sync
-        aiocron.crontab(f'*/{env.SYNC_INTERVAL_MINUTES} * * * *', func=sync_scrobble_vault)
-        
-        logging.info(f"Scheduled sync every {env.SYNC_INTERVAL_MINUTES} minutes")
-        
-        # Keep the event loop running forever
-        await asyncio.Event().wait()
+        async with asyncio.TaskGroup() as tg:
+            tg.create_task(start_scrobble_vault())
+            tg.create_task(start_api())
+
     finally:
         # Clean up database connection pool on shutdown
         await close_db()
