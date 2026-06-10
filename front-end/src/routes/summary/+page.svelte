9efd1207a1270d7c49dc01727f7e55cf24c49cab
Benson Chow <email@bensonc.how>
2026-06-10 19:06:33 +0100

    feat(scrobble_vault-frontend): Add top charts section

--- front-end/src/routes/summary/+page.svelte
+++ front-end/src/routes/summary/+page.svelte
@@ -1,11 +1,14 @@
 <!-- scrobble vault /summary endpoint frontend -->
 <script lang="ts">
+	// import utils used
     import { timeAgo, formatHour } from '$lib/utils';
+
+	// import icons
 	import Clock from '$lib/icons/clock.svelte';
 	import Calender from '$lib/icons/calendar.svelte';
 	import Bulb from '$lib/icons/bulb.svelte';
-    import Calendar from '$lib/icons/calendar.svelte';
-
+	
+	// handle data
     let { data } = $props(); // runs the load function in page.server.ts
     let selectedPeriod = $state(0); // reactive variable, default is 7 days
 
@@ -39,7 +42,7 @@
                     px-4 py-2 rounded-lg text-sm
                     {	
                         selectedPeriod === i
-                        ? 'bg-violet-600 text-white' // activated if button active
+                        ? 'bg-violet-500 text-white' // activated if button active
                         : 'text-white/40 hover:text-white/50 bg-white/5 hover:bg-white/20' // activate if not active
                     }
                 "
@@ -59,31 +62,31 @@
         <!-- Overview row 1 & 2, use grid for uniformed sized cards -->
         <div class="grid grid-cols-2 md:grid-cols-4 mt-2 gap-2">
 			<!--Scrobble stats-->
-			<div class="bg-[#131311] border border-violet-600/50 rounded-lg p-3">
+			<div class="bg-[#131311] border border-violet-500/50 rounded-lg p-3">
 				<p class="text-sm text-white/70">Tracks listened</p>
 				<p class="text-base text-white mt-1">{stats.total_scrobbles.toLocaleString()}</p>
 				<p class="text-xs text-white/50">{stats.active_days} active days</p>
 			</div>
 			<!--Total unique artists, inviside third row so card size consistant-->
-			<div class="bg-[#131311] border border-violet-600/50 rounded-lg p-3">
+			<div class="bg-[#131311] border border-violet-500/50 rounded-lg p-3">
 				<p class="text-sm text-white/70">Unique artists</p>
 				<p class="text-base text-white mt-1">{stats.unique_artists_count.toLocaleString()}</p>
 				<p class="text-xs invisible">.</p>
 			</div>
 			<!--Total unique albumns, inviside third row so card size consistant-->
-			<div class="bg-[#131311] border border-violet-600/50 rounded-lg p-3">
+			<div class="bg-[#131311] border border-violet-500/50 rounded-lg p-3">
 				<p class="text-sm text-white/70">Unique albums</p>
 				<p class="text-base text-white mt-1">{stats.unique_albums_count.toLocaleString()}</p>
 				<p class="text-xs invisible">.</p>
 			</div>
 			<!--Total unique albumns, inviside third row so card size consistant-->
-			<div class="bg-[#131311] border border-violet-600/50 rounded-lg p-3">
+			<div class="bg-[#131311] border border-violet-500/50 rounded-lg p-3">
 				<p class="text-sm text-white/70">Unique tracks</p>
 				<p class="text-base text-white mt-1">{stats.unique_tracks_count.toLocaleString()}</p>
 				<p class="text-xs invisible">.</p>
 			</div>
 			<!--Listening time-->
-			<div class="bg-[#131311] border border-violet-600/50 rounded-lg p-3">
+			<div class="bg-[#131311] border border-violet-500/50 rounded-lg p-3">
 				<p class="text-sm text-white/70">Listening time</p>
                 {#if stats.listening_time && stats.listening_time.total_string}
                     <p class="text-base text-white mt-1">{stats.listening_time.total_string}</p>
@@ -96,7 +99,7 @@
 			</p>
 			</div>
 			<!--First scrobble,inviside third row so card size consistant-->
-			<div class="bg-[#131311] border border-violet-600/50 rounded-lg p-3">
+			<div class="bg-[#131311] border border-violet-500/50 rounded-lg p-3">
 				<p class="text-sm text-white/70">First track recorded</p>
 				{#if stats.first_listened_at}
 					<p class="text-base text-white mt-1">
@@ -111,7 +114,7 @@
 				{/if}
 			</div>
 			<!--Last scrobble, inviside third row so card size consistant-->
-			<div class="bg-[#131311] border border-violet-600/50 rounded-lg p-3">
+			<div class="bg-[#131311] border border-violet-500/50 rounded-lg p-3">
 				<p class="text-sm text-white/70">Last track recorded</p>
 				{#if stats.last_listened_at}
 					<p class="text-base text-white mt-1">
@@ -126,7 +129,7 @@
 				{/if}
 			</div>
 			<!--Most active day, inviside third row so card size consistant-->
-			<div class="bg-[#131311] border border-violet-600/50 rounded-lg p-3">
+			<div class="bg-[#131311] border border-violet-500/50 rounded-lg p-3">
 				<p class="text-sm text-white/70">Most active day</p>
 				{#if stats.most_active_day}
 					<p class="text-base text-white mt-1">
@@ -147,7 +150,7 @@
         <!-- Overview row 3 (the highlight cards)-->
 		<div class="grid grid-cols-1 sm:grid-cols-3 gap-2 mt-2">
 			<!--Peak hour stats-->
-			<div class="bg-[#131311] border border-violet-600/50 rounded-lg p-3">
+			<div class="bg-[#131311] border border-violet-500/50 rounded-lg p-3">
 				<div class="text-violet-400  [&>svg]:w-4 [&>svg]:h-4">
 					<Clock/>
 				</div>
@@ -165,9 +168,9 @@
 				{/if}
 			</div>
 			<!--Peak hour stats-->
-			<div class="bg-[#131311] border border-violet-600/50 rounded-lg p-3">
+			<div class="bg-[#131311] border border-violet-500/50 rounded-lg p-3">
 				<div class="text-violet-400  [&>svg]:w-4 [&>svg]:h-4">
-					<Calendar/>
+					<Calender/>
 				</div>
 				<p class="text-sm text-white/70 mt-1">Average peak weekday</p>
 				{#if stats.listening_clock.peak_hour}
@@ -183,7 +186,7 @@
 				{/if}
 			</div>
 			<!--Discovery stats-->
-			<div class="bg-[#131311] border border-violet-600/50 rounded-lg p-3">
+			<div class="bg-[#131311] border border-violet-500/50 rounded-lg p-3">
 				<div class="text-violet-400 [&>svg]:w-4 [&>svg]:h-4">
 					<Bulb/>
 				</div>
@@ -206,9 +209,153 @@
 			</div>
 		</div>
     </section>
-    
-    <!-- top tracks + artists -->
-    <!-- top albums -->
+
+	<!-- The white seperator -->
+	<hr class="my-4 border-t border-white/20"/>
+
+	<!-- Top artists & albumns & tracks -->
+	<section>
+		<p class="text-white text-lg">TOP CHARTS</p>
+		
+		<div class="grid grid-cols-1 md:grid-cols-2 gap-8">
+			<!-- Top artists -->
+			<div>
+				<p class="text-sm text-white/40 mt-2 mb-2">Artists</p>
+				{#each stats.top_artists as artist, i}
+					{@const maxPlays = stats.top_artists[0].plays}
+					{@const artistImage = artist.artist_image_extralarge ?? artist.artist_image_large ?? artist.artist_image_medium ?? artist.artist_image_small}
+					
+					<!-- The bar is an absolute positioned element to take up no space, relative make it anchored to this div instead -->
+					<!-- Item center makes all items except the bar centered vertically-->
+					<div class="relative flex items-center gap-3 py-3">
+						<!--The ranking numbers, w-4 makes the double digit numbers rows not inconsistent-->
+						<span class="text-xs w-4 text-right {i < 3 ? 'text-violet-400' : 'text-white/30'}">
+							{i + 1}
+						</span>
+						
+						<!-- The artist image -->
+						{#if artistImage}
+							<!--The classes set perfered size, round corners, fill perfer size then prevent flex from squishing it-->
+							<img 
+								src={artistImage} alt={artist.artist_name}
+								referrerpolicy="no-referrer"
+								class="w-9 h-9 rounded-md object-cover shrink-0"
+							/>
+						{:else}
+							<!--If no image display the first letter of artist name-->
+							<!--Same as the image, but instead of img, create another flex container and center in both axis, in it display the text -->
+							<div class="w-9 h-9 rounded-md bg-violet-500/10 shrink-0 flex items-center justify-center text-violet-400 text-sm font-medium">
+								{artist.artist_name[0].toUpperCase()}
+							</div>
+						{/if}
+
+						<!-- Artist name, truncate if too long, take up all remaining space, min-w-0 prevent overflow and prevent the play count off screen-->
+						<div class="min-w-0 flex-1">
+							<p class="text-sm text-white truncate">{artist.artist_name}</p>
+						</div>
+
+						<!-- Play count-->
+						<span class="text-xs text-white/30">{artist.plays.toLocaleString()} plays</span>
+
+						<!-- Play count comparison bar, pin to very bottom edge of parent, set to 1px height and use background colour to show unfilled portion. -->
+						<div class="absolute bottom-0 left-0 right-0 h-px bg-white/5">
+							<!-- Filled portion, fill entiere 1 px, and round the bar corners -->
+							<div class="h-full bg-violet-500 rounded-full"
+								style="width: {(artist.plays / maxPlays) * 100}%">
+							</div>
+						</div>
+					</div>
+				{/each}
+			</div>
+			<!-- Top albums, see artist chart for documentation-->
+			<div>
+				<p class="text-sm text-white/40 mt-2 mb-2">Albums</p>
+				{#each stats.top_albums as album, i}
+					{@const maxPlays = stats.top_albums[0].plays}
+					{@const albumImage = album.album_image_extralarge ?? album.album_image_large ?? album.album_image_medium ?? album.album_image_small}
+
+					<div class="relative flex items-center gap-3 py-3">
+						<span class="text-xs w-4 text-right {i < 3 ? 'text-violet-400' : 'text-white/30'}">
+							{i + 1}
+						</span>
+
+						{#if albumImage}
+							<img 
+								src={albumImage} alt={album.album_name}
+								referrerpolicy="no-referrer"
+								class="w-9 h-9 rounded-md object-cover shrink-0"
+							/>
+						{:else}
+							<div class="w-9 h-9 rounded-md bg-violet-500/10 shrink-0 flex items-center justify-center text-violet-400 text-sm font-medium">
+								{album.album_name[0].toUpperCase()}
+							</div>
+						{/if}
+
+						<div class="min-w-0 flex-1">
+							<p class="text-sm text-white truncate">{album.album_name}</p>
+							<p class="text-xs text-white/40 truncate">{album.artist_name}</p>
+						</div>
+
+						<span class="text-xs text-white/30">{album.plays.toLocaleString()} plays</span>
+
+						<div class="absolute bottom-0 left-0 right-0 h-px bg-white/5">
+							<div class="h-full bg-violet-500 rounded-full"
+								style="width: {(album.plays / maxPlays) * 100}%">
+							</div>
+						</div>
+					</div>
+				{/each}
+			</div>
+		</div>
+		<!-- Top tracks, see artist chart for documentation -->
+		<div>
+			<p class="text-sm text-white/40 mt-2 mb-2">Tracks</p>
+			{#each stats.top_tracks as track, i}
+				{@const maxPlays = stats.top_tracks[0].plays}
+				{@const trackImage = 
+				track.album_image_extralarge ?? 
+				track.album_image_large ?? 
+				track.album_image_medium ?? 
+				track.album_image_small ?? 
+				track.artist_image_extralarge ?? 
+				track.artist_image_large ?? 
+				track.artist_image_medium ?? 
+				track.artist_image_small}
+
+				<div class="relative flex items-center gap-3 py-3">
+					<span class="text-xs w-4 text-right {i < 3 ? 'text-violet-400' : 'text-white/30'}">
+						{i + 1}
+					</span>
+
+					{#if trackImage}
+						<img 
+							src={trackImage} alt={track.track_name}
+							referrerpolicy="no-referrer"
+							class="w-9 h-9 rounded-md object-cover shrink-0"
+						/>
+					{:else}
+						<div class="w-9 h-9 rounded-md bg-violet-500/10 shrink-0 flex items-center justify-center text-violet-400 text-sm font-medium">
+							{track.track_name[0].toUpperCase()}
+						</div>
+					{/if}
+
+					<!-- Also show artist name -->
+					<div class="min-w-0 flex-1">
+						<p class="text-sm text-white truncate">{track.track_name}</p>
+						<p class="text-xs text-white/40 truncate">{track.artist_name}</p>
+					</div>
+
+					<span class="text-xs text-white/30">{track.plays.toLocaleString()} plays</span>
+
+					<div class="absolute bottom-0 left-0 right-0 h-px bg-white/5">
+						<div class="h-full bg-violet-500 rounded-full"
+							style="width: {(track.plays / maxPlays) * 100}%">
+						</div>
+					</div>
+				</div>
+			{/each}
+		</div>
+	</section>
     <!-- new discoveries -->
     <!-- listening clock -->
     <!-- recent tracks -->
